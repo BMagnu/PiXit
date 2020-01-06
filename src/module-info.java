6f4da21a713452f0a5165cf784dddcd1dfa7552c
Birk Magnussen <birk.magnussen@online.de>
2020-01-06 18:51:32 +0100

    JDK11

---
+++ src/module-info.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+/**
+ * @author Birk
+ *
+ */
+module pixit {
+	
+	exports net.bmagnu.pixit.common;
+	
+	exports net.bmagnu.pixit.server.handlers;
+	
+	exports net.bmagnu.pixit.server;
+	
+	exports net.bmagnu.pixit.client;
+	
+	exports net.bmagnu.pixit.client.handlers;
+	
+	requires activation;
+	
+	requires javafx.base;
+	
+	requires javafx.controls;
+	
+	requires javafx.fxml;
+	
+	requires javafx.graphics;
+	
+	requires json.simple;
+}
